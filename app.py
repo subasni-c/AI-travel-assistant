9276324bbfb6b2c383c09aa663c72cc76a3770b4
subasni-c <subasnisubasni23@gmail.com>
2026-04-10 23:01:30 +0530

    update fastapi url

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import requests
 import uuid
 
-FASTAPI_URL = "http://localhost:8000"
+FASTAPI_URL = "https://ai-travel-assistant-wo0c.onrender.com"
 
 st.set_page_config(
     page_title="AI Travel Assistant",
